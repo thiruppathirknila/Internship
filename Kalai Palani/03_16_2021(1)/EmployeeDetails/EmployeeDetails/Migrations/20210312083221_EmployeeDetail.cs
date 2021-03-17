e92a5e4d8711bb2699b7dcb1db96fd6d2c8ca795
Bhuvaneswari <ganeshkumar.r@knila.com>
2021-03-17 11:53:57 +0530

    webapi&angular

---
+++ Kalai Palani/03_16_2021(1)/EmployeeDetails/EmployeeDetails/Migrations/20210312083221_EmployeeDetail.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeDetails.Migrations
+{
+    public partial class EmployeeDetail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeName = table.Column<string>(nullable: true),
+                    Gender = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
