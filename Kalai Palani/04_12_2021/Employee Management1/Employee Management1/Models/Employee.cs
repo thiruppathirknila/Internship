a56e431cb3905b8800916e8ac047c63b3719c717
Bhuvaneswari <ganeshkumar.r@knila.com>
2021-04-13 12:32:43 +0530

    Employee Management

---
+++ Kalai Palani/04_12_2021/Employee Management1/Employee Management1/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee_Management1.Models
+{
+    [Table("Employees")]
+    public class Employee
+    {
+        
+        [Key]
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string Department { get; set; }
+        public string Designation { get; set; }
+        public DateTime DOB { get; set; }
+        public decimal Salary { get; set; }
+
+    }
+}
