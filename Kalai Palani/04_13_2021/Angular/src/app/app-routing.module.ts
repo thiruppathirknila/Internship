ac272bc408fbf182a59f4c9ad287cab6dc301063
Bhuvaneswari <ganeshkumar.r@knila.com>
2021-04-13 15:43:18 +0530

    Angular

---
+++ Kalai Palani/04_13_2021/Angular/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { EmployeeService } from './employee.auth';
+import { EmployeeComponent } from './employee/employee.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+ 
+      {path: '', component: LoginComponent },
+      {path: 'login', component: LoginComponent },
+      {path: 'employee', component: EmployeeComponent, canActivate: [EmployeeService] },
+      {path:'add-employee',component:AddEmployeeComponent},
+      {path: 'edit',component:AddEmployeeComponent},
+
+     { path: '', redirectTo: 'employee', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Kalai Palani/04_13_2021/Angular/src/app/employee.auth.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService implements CanActivate {
+
+  constructor(private jwtHelper: JwtHelperService, private router: Router) { }
+  canActivate() {
+    const token = localStorage.getItem("jwt");
+
+    if (token && !this.jwtHelper.isTokenExpired(token)){
+      console.log(this.jwtHelper.decodeToken(token));
+      return true;
+    }
+    this.router.navigate(["/login"]);
+    return false;
+}
+}
