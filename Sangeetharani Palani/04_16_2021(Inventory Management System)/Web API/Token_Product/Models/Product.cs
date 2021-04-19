72321b92142974cc8ef57479fb01dbaa799c647b
Arul <arulvelavan.g@knila.com>
2021-04-19 12:43:28 +0530

    Web API

---
+++ Sangeetharani Palani/04_16_2021(Inventory Management System)/Web API/Token_Product/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Token_Product.Models
+{
+    [Table("tbl_Products")]
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public decimal Total { get; set; }
+       
+
+    }
+}
