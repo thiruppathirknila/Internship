cc638d3b8fa78853787e00f0c41cc4cb43696d1e
Bhuvaneswari <ganeshkumar.r@knila.com>
2021-04-21 10:18:25 +0530

    Webapi

---
+++ Kalai Palani/04_20_2021/WebApi/Employee Management1/Controllers/ApplyLeaveController.cs
@@ -0,0 +1,23 @@
+﻿using Employee_Management1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee_Management1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplyLeaveController : ControllerBase
+    {
+        private readonly EmployeeContext _context;
+
+        public ApplyLeaveController(EmployeeContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
---
+++ Kalai Palani/04_20_2021/WebApi/Employee Management1/Models/ApplyLeave.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee_Management1.Models
+{
+    public class ApplyLeave
+    {
+        public string EmployeeName { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string LeaveType { get; set; }
+        public string Comments { get; set; }
+    }
+}
