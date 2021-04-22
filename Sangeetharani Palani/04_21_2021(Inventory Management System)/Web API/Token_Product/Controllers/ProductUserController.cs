217e4c416ef791e4762b33671a93db1ecf15dda8
Arul <arulvelavan.g@knila.com>
2021-04-22 12:57:22 +0530

    Web API

---
+++ Sangeetharani Palani/04_21_2021(Inventory Management System)/Web API/Token_Product/Controllers/ProductUserController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Token_Product.Models;
+
+namespace Token_Product.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ProductUserController : ControllerBase
+    {
+        [HttpPost, Route("login")]
+
+        public IActionResult Login([FromBody] Login user)
+        {
+            if (user == null)
+                return BadRequest("Invalid Client Request");
+
+            if (user.Username == "test" && user.Password == "test")
+            {
+                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SecretKeyWord@123"));
+                var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+                var tokenOptions = new JwtSecurityToken(
+                    issuer: "https://localhost:44384",
+                    audience: "https://localhost:44384",
+                    claims: new List<Claim>(),
+                    expires: DateTime.Now.AddDays(1),
+                    signingCredentials: signingCredentials
+
+                    );
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+                return Ok(new { Token = tokenString });
+
+            }
+
+            return Unauthorized();
+        }
+    }
+}
+
---
+++ Sangeetharani Palani/04_21_2021(Inventory Management System)/Web API/Token_Product/Models/ProductContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Token_Product.Models
+{
+    public class ProductContext:DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+   //     public DbSet<Invoice> Invoices { get; set; }
+
+    }
+}
